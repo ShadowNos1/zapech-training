fb7ba1ece2c784bdb1d4bd472e59c3c35436d06c
ShadowNos1 <nosakin.andrei@yandex.ru>
2026-02-27 12:32:09 +0300

    1.24 pages

--- src/components/ProfessionSelect.jsx
+++ src/components/ProfessionSelect.jsx
@@ -42,9 +42,7 @@ function ProfessionSelect({ onSelect }) {
           );
         })}
       </div>
-      <div className="mt-10 text-center text-slate-400 text-xs">
-        © 2026 ЗАПЕЧЬ. Все права защищены.
-      </div>
+      
     </div>
   );
 }
